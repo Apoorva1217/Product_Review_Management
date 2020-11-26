f58cbf96f39211518ef313b3039ad2e008961d89
Apoorva1217 <apoorvarasal12@gmail.com>
2020-11-26 23:23:42 +0530

    [Apoorva] Add . Welcome to Product Review Management using LINQ

---
+++ ProductReviewManagementLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagementLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management using LINQ!");
+        }
+    }
+}
